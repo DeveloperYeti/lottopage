1f17ec129b6cde35b5f4ba8947c9f66a5f2a7849
DeveloperYeti <hoseok9823@gmail.com>
2025-11-16 17:36:33 +0900

    입력 값 로직 구현. input()

--- src/main/java/com/lottopage/springboot/controller/LottoController.java
+++ src/main/java/com/lottopage/springboot/controller/LottoController.java
@@ -1,6 +1,15 @@
 package com.lottopage.springboot.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoController {
 
+    // 입력 값 반환 - Scanner 역할
+    public static String input(){
+        return Console.readLine();
+    }
+    
 
 }
+
+
