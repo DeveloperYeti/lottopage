8b7a5337885418e9c5c598aefe1814ea18538527
DeveloperYeti <hoseok9823@gmail.com>
2025-11-17 23:34:47 +0900

    user 테이블에서 회원가입 시 비밀번호 , email, 아이디 중복 확인 로직

--- src/main/java/com/lottopage/springboot/repository/UserRepository.java
+++ src/main/java/com/lottopage/springboot/repository/UserRepository.java
@@ -3,4 +3,8 @@ package com.lottopage.springboot.repository;
 import com.lottopage.springboot.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> { }
+public interface UserRepository extends MongoRepository<User, String> {
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+    User findByUsername(String username);
+}
