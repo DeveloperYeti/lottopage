11a2b1fefb6c91cad707eaa53da39b01d4c6d43b
DeveloperYeti <hoseok9823@gmail.com>
2025-11-17 23:33:40 +0900

    user 회원가입 로그인 로직에 따른 getter setter 값 추가.

--- src/main/java/com/lottopage/springboot/domain/User.java
+++ src/main/java/com/lottopage/springboot/domain/User.java
@@ -7,15 +7,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id;
-    private String name;
     private String email;
+    private String name;
+    private String username; // 사용자 ID
+    private String password;
 
     public User() {}
-
-    public User(String name, String email) {
-        this.name = name;
+    public User(String email, String name, String username, String password) {
         this.email = email;
+        this.name = name;
+        this.username = username;
+        this.password = password;
     }
+
 
     // Getter, Setter
     public String getId() { return id; }
@@ -24,4 +28,20 @@ public class User {
     public void setName(String name) { this.name = name; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
